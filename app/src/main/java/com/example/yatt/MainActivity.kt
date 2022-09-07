0b3415c4b769afe1d780bb3b77ee75910c9e7846
TxMat <Mathieu.ponal@gmail.com>
2022-09-07 16:16:13 -0400

    app center config

--- app/src/main/java/com/example/yatt/MainActivity.kt
+++ app/src/main/java/com/example/yatt/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.yatt
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AppCenter.start(
+            application, "991e68d2-101e-4136-bd1c-1c9f9a904773",
+            Analytics::class.java, Crashes::class.java
+        )
         setContentView(R.layout.activity_main)
     }
 }
